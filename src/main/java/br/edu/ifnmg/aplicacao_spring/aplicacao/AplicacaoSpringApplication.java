7f4a68e79d21d60801569f9e0bcf30113b85163e
Matheus Freitas <matheusdefreitask9@hotmail.com>
2023-03-21 02:28:17 -0300

    correção do bean

--- src/main/java/br/edu/ifnmg/aplicacao_spring/aplicacao/AplicacaoSpringApplication.java
+++ src/main/java/br/edu/ifnmg/aplicacao_spring/aplicacao/AplicacaoSpringApplication.java
@@ -1,14 +1,34 @@
 package br.edu.ifnmg.aplicacao_spring.aplicacao;
 
+import br.edu.ifnmg.aplicacao_spring.entidades.Usuario;
+import br.edu.ifnmg.aplicacao_spring.servicos.UsuarioDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @EntityScan("br.edu.ifnmg.aplicacao_spring.entidades")
+@ComponentScan("br.edu.ifnmg.aplicacao_spring")
 @SpringBootApplication
-public class AplicacaoSpringApplication {
+public class AplicacaoSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplicacaoSpringApplication.class, args);
 	}
+
+	@Autowired
+	UsuarioDAO usuarios;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario u = new Usuario();
+		u.setLogin("matheus");
+		u.setPassword("123");
+
+		usuarios.salvar(u);
+	}
+
 }
--- src/main/java/br/edu/ifnmg/aplicacao_spring/infraestrutura/GenericDAOImpl.java
+++ src/main/java/br/edu/ifnmg/aplicacao_spring/infraestrutura/GenericDAOImpl.java
@@ -5,12 +5,10 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
-import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 
-@Repository
 public class GenericDAOImpl<T, ID> implements GenericDAO<T, ID> {
 
     @PersistenceContext

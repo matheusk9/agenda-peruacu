50f79a182797e92c5bde9bde2b90ef66720e5f8d
Matheus Freitas <matheusdefreitask9@hotmail.com>
2023-03-29 20:40:47 -0300

    add MainController

---
+++ src/main/java/br/edu/ifnmg/aplicacao_spring/controller_javafx/MainController.java
@@ -0,0 +1,86 @@
+package br.edu.ifnmg.aplicacao_spring.controller_javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainController {
+
+    @FXML
+    private Button btnAdicionarAdm;
+
+    @FXML
+    private Button btnAdicionarGuia;
+
+    @FXML
+    private Button btnAdicionarNovo;
+
+    @FXML
+    private TableView<?> tbViewADM;
+
+    @FXML
+    private TableView<?> tbViewGuias;
+
+    @FXML
+    private TableView<?> tbViewVisitas;
+
+    @FXML
+    private TableColumn<?, ?> tcAcao;
+
+    @FXML
+    private TableColumn<?, ?> tcAcaoADM;
+
+    @FXML
+    private TableColumn<?, ?> tcAcaoGuia;
+
+    @FXML
+    private TableColumn<?, ?> tcAtrativo;
+
+    @FXML
+    private TableColumn<?, ?> tcCPFADM;
+
+    @FXML
+    private TableColumn<?, ?> tcCPFGuia;
+
+    @FXML
+    private TableColumn<?, ?> tcCpf;
+
+    @FXML
+    private TableColumn<?, ?> tcData;
+
+    @FXML
+    private TableColumn<?, ?> tcNome;
+
+    @FXML
+    private TableColumn<?, ?> tcNomeADM;
+
+    @FXML
+    private TableColumn<?, ?> tcNomeGuia;
+
+    @FXML
+    private TableColumn<?, ?> tcTelefone;
+
+    @FXML
+    private TableColumn<?, ?> tcTelefoneADM;
+
+    @FXML
+    private TableColumn<?, ?> tcTelefoneGuia;
+
+    @FXML
+    void adicionarNovoAdm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void adicionarNovosGuias(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agendarNovaVisita(ActionEvent event) {
+
+    }
+
+}
